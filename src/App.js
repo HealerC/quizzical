aae91c99dd91f849ce176fbd231f02337e8c1cec
Uyi <ihazauyi@gmail.com>
2022-06-02 21:11:38 +0100

    feat: Initialize router implementation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import WelcomeScreen from "./components/WelcomeScreen";
 import StartGameSettings from "./components/StartGameSettings";
 import Game from "./components/Game";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <WelcomeScreen />
-      <StartGameSettings />
-      <Game />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/settings" element={<StartGameSettings />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="*" element={<div>404 page not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
