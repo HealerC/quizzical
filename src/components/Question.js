7de8294cdfad6868fdb1cae506e39ec485f92587
Uyi <ihazauyi@gmail.com>
2022-06-06 23:54:27 +0100

    feat: Control input on choosing options

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,40 @@
 import React from "react";
 import { nanoid } from "nanoid";
 
-const Question = ({ trivia }) => {
-  const { question, options } = trivia;
+const Question = ({ trivia: quizQuestion }) => {
+  const [trivia, setTrivia] = React.useState({
+    question: "",
+    correct_answer: "",
+    options: [],
+    selected: "",
+    category: "",
+    difficulty: "",
+  });
+
+  React.useEffect(() => {
+    setTrivia(quizQuestion);
+    console.log(quizQuestion);
+  }, [quizQuestion]);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setTrivia((trivia) => ({ ...trivia, selected: value }));
+  };
+  console.log(trivia);
   return (
     <article>
-      <h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
-      {options.map((option) => {
+      <h2 dangerouslySetInnerHTML={{ __html: trivia.question }}></h2>
+      {trivia.options.map((option) => {
         return (
           <span key={nanoid()}>
-            <input type="radio" name={question} value={option} />
+            <input
+              type="radio"
+              name={trivia.question}
+              value={option}
+              id={option}
+              onChange={handleChange}
+              checked={option === trivia.selected}
+            />
             <label
               htmlFor={option}
               dangerouslySetInnerHTML={{ __html: option }}
