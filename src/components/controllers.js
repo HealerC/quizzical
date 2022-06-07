e902d485f9a9ce1cb122524db0d704c210877468
Uyi <ihazauyi@gmail.com>
2022-06-07 15:09:03 +0100

    feat: Add an id to each question

--- src/components/controllers.js
+++ src/components/controllers.js
@@ -1,9 +1,13 @@
+//import { nanoid } from "nanoid";
+const { nanoid } = require("nanoid");
+
 const createGame = (apiResult) => {
   const game = apiResult.map((result) => {
     const { category, difficulty, question, correct_answer } = result;
     const options = result.incorrect_answers.concat(correct_answer);
     options.sort((a, b) => 0.5 - Math.random());
     const selected = "";
+    const id = nanoid();
     return {
       question,
       correct_answer,
@@ -11,11 +15,10 @@ const createGame = (apiResult) => {
       category,
       difficulty,
       selected,
+      id,
     };
   });
   return game;
 };
 
-module.exports = {
-  createGame,
-};
+module.exports = { createGame };
