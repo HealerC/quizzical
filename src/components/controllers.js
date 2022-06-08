bd10a20842c150344d186269926f57ba6683cd85
Uyi <ihazauyi@gmail.com>
2022-06-08 12:26:04 +0100

    feat: Add a converter to convert from milliseconds to minutes:seconds and from minutes to milliseconds

--- src/components/controllers.js
+++ src/components/controllers.js
@@ -40,4 +40,25 @@ const createGame = (apiResult) => {
   return game;
 };
 
-module.exports = { getGame };
+const TimeConverter = {
+  minutesToMillis(minutes) {
+    return minutes * 60 * 1000;
+  },
+  millisToMinuteSecond(millis) {
+    const seconds = millis / 1000;
+
+    let minutes = Math.floor(seconds / 60).toString();
+    let secondsRemaining = (seconds % 60).toString();
+
+    if (minutes.length < 2) {
+      minutes = "0" + minutes;
+    }
+    if (secondsRemaining.length < 2) {
+      secondsRemaining = "0" + secondsRemaining;
+    }
+
+    return `${minutes}:${secondsRemaining}`;
+  },
+};
+
+module.exports = { getGame, TimeConverter };
