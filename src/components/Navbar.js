4601caf5982024c9681e4dc26d5ef0b1b4fbacd8
Uyi <ihazauyi@gmail.com>
2022-06-14 18:58:05 +0100

    feat: Initialize styling of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
-      <NavLink to="/game">Game</NavLink>
+      <NavLink to="/" className={({ isActive }) => (isActive ? "link active" : "link")}>Home</NavLink>
+      <NavLink to="/settings" className={({ isActive }) => (isActive ? "link active" : "link")}>Settings</NavLink>
+      <NavLink to="/game" className={({ isActive }) => (isActive ? "link active" : "link")}>Game</NavLink>
     </nav>
   );
 };
