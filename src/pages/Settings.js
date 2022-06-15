24512450afe63a380cd7a87428db8823979e82df
Uyi <ihazauyi@gmail.com>
2022-06-15 12:52:59 +0100

    feat: Style radio buttons in /settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -115,103 +115,139 @@ const Settings = ({ setGame }) => {
         </fieldset>
         <fieldset name="difficulty">
           <legend>Difficulty</legend>
-          <input
-            type="radio"
-            name="difficulty"
-            value="any"
-            id="any-difficulty"
-            onChange={handleChange}
-            checked={settings.difficulty === "any"}
-          />
-          <label htmlFor="any-difficulty">Any</label>
-          <input
-            type="radio"
-            name="difficulty"
-            value="easy"
-            id="easy-difficulty"
-            onChange={handleChange}
-            checked={settings.difficulty === "easy"}
-          />
-          <label htmlFor="easy-difficulty">Easy</label>
-          <input
-            type="radio"
-            name="difficulty"
-            value="medium"
-            id="medium-difficulty"
-            onChange={handleChange}
-            checked={settings.difficulty === "medium"}
-          />
-          <label htmlFor="medium-difficulty">Medium</label>
-          <input
-            type="radio"
-            name="difficulty"
-            value="hard"
-            id="hard-difficulty"
-            onChange={handleChange}
-            checked={settings.difficulty === "hard"}
-          />
-          <label htmlFor="hard-difficulty">Hard</label>
+          <div className="radio-container">
+            <label className="container">
+              Any
+              <input
+                type="radio"
+                name="difficulty"
+                value="any"
+                id="any-difficulty"
+                onChange={handleChange}
+                checked={settings.difficulty === "any"}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              Easy
+              <input
+                type="radio"
+                name="difficulty"
+                value="easy"
+                id="easy-difficulty"
+                onChange={handleChange}
+                checked={settings.difficulty === "easy"}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              Medium
+              <input
+                type="radio"
+                name="difficulty"
+                value="medium"
+                id="medium-difficulty"
+                onChange={handleChange}
+                checked={settings.difficulty === "medium"}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              Hard
+              <input
+                type="radio"
+                name="difficulty"
+                value="hard"
+                id="hard-difficulty"
+                onChange={handleChange}
+                checked={settings.difficulty === "hard"}
+              />
+              <span className="mark"></span>
+            </label>
+          </div>
         </fieldset>
         <fieldset name="question-count">
           <legend>No. of questions</legend>
-          <input
-            type="radio"
-            name="questionCount"
-            value="5"
-            id="5qc"
-            onChange={handleChange}
-            checked={settings.questionCount === 5}
-          />
-          <label htmlFor="5qc">5</label>
-          <input
-            type="radio"
-            name="questionCount"
-            value="10"
-            id="10qc"
-            onChange={handleChange}
-            checked={settings.questionCount === 10}
-          />
-          <label htmlFor="10qc">10</label>
-          <input
-            type="radio"
-            name="questionCount"
-            value="20"
-            id="20qc"
-            onChange={handleChange}
-            checked={settings.questionCount === 20}
-          />
-          <label htmlFor="20qc">20</label>
+          <div className="radio-container">
+            <label className="container">
+              5
+              <input
+                type="radio"
+                name="questionCount"
+                value="5"
+                id="5qc"
+                onChange={handleChange}
+                checked={settings.questionCount === 5}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              10
+              <input
+                type="radio"
+                name="questionCount"
+                value="10"
+                id="10qc"
+                onChange={handleChange}
+                checked={settings.questionCount === 10}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              20
+              <input
+                type="radio"
+                name="questionCount"
+                value="20"
+                id="20qc"
+                onChange={handleChange}
+                checked={settings.questionCount === 20}
+              />
+              <span className="mark"></span>
+            </label>
+          </div>
         </fieldset>
 
         <fieldset name="time">
           <legend>Time (mins)</legend>
-          <input
-            type="radio"
-            name="time"
-            value="10"
-            id="10t"
-            onChange={handleChange}
-            checked={settings.time === 10}
-          />
-          <label htmlFor="10t">10</label>
-          <input
-            type="radio"
-            name="time"
-            value="20"
-            id="20t"
-            onChange={handleChange}
-            checked={settings.time === 20}
-          />
-          <label htmlFor="20t">20</label>
-          <input
-            type="radio"
-            name="time"
-            value="40"
-            id="40t"
-            onChange={handleChange}
-            checked={settings.time === 40}
-          />
-          <label htmlFor="40t">40</label>
+          <div className="radio-container">
+            <label className="container">
+              10
+              <input
+                type="radio"
+                name="time"
+                value="10"
+                id="10t"
+                onChange={handleChange}
+                checked={settings.time === 10}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              20
+              <input
+                type="radio"
+                name="time"
+                value="20"
+                id="20t"
+                onChange={handleChange}
+                checked={settings.time === 20}
+              />
+              <span className="mark"></span>
+            </label>
+            <label className="container">
+              40
+              <input
+                type="radio"
+                name="time"
+                value="40"
+                id="40t"
+                onChange={handleChange}
+                checked={settings.time === 40}
+              />
+              <span className="mark"></span>
+            </label>
+          </div>
         </fieldset>
 
         {categories ? (
