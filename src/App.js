b5fcbbec451692e1f44d56b84f2c4ecfc6d6b95e
Uyi <ihazauyi@gmail.com>
2022-06-16 20:37:55 +0100

    refactor: Remove code from the old state implementation/other unneccessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
 import ProtectedSettings from "./pages/ProtectedSettings";
 import Game from "./pages/Game";
 import SharedLayout from "./components/SharedLayout";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { DEFAULT_STATE, quizzicalReducer } from "./components/reducer";
-//import QuizzicalContext from "./components/QuizzicalContext";
+
 import { QuizzicalContext } from "./components/QuizzicalContext";
 import "./stylesheets/app.css";
 
 const App = () => {
-  const [game, setGame] = React.useState({
-    username: "",
-    game: [],
-    url: "",
-    time: 0,
-  });
-
-  //const [state, dispatch] = React.useReducer(DEFAULT_STATE, quizzicalReducer);
   const { state, dispatch } = React.useContext(QuizzicalContext);
+
   React.useEffect(() => {
     try {
       const storage = window["localStorage"];
@@ -30,7 +24,6 @@ const App = () => {
       const lastSession = storage.getItem("quizzical");
       if (lastSession) {
         const lastGameData = JSON.parse(lastSession);
-        console.log("something here last time", lastGameData);
         dispatch({ type: "LOAD_PREV_GAME", payload: lastGameData });
       }
     } catch (error) {
@@ -38,7 +31,7 @@ const App = () => {
       dispatch({ type: "NO_STORAGE" });
     }
   }, []);
-  console.log("app", state);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -48,16 +41,15 @@ const App = () => {
             path="settings"
             element={
               <ProtectedSettings>
-                <Settings setGame={setGame} />
+                <Settings />
               </ProtectedSettings>
             }
           />
-          <Route path="game" element={<Game game={game} />} />
+          <Route path="game" element={<Game />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 };
-// export { QuizzicalContext };
 export default App;
