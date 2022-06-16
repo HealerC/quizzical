29cff8fac27f17e6684d465c8961a931b65a06e7
Uyi <ihazauyi@gmail.com>
2022-06-16 21:34:40 +0100

    style: Add few comments on question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,8 @@ import React from "react";
 import { nanoid } from "nanoid";
 
 const Question = ({ trivia, handleChange, status }) => {
+  /* When the game is over, show the correct option in green 
+  but if user selected a wrong option, show it in red */
   const showCorrectAnswer = (option) => {
     if (option === trivia.correct_answer) {
       return <span className="mark correct"></span>;
@@ -9,14 +11,20 @@ const Question = ({ trivia, handleChange, status }) => {
       return <span className="mark incorrect"></span>;
     }
   };
+
   return (
     <article className="question">
+      {/* html was dangerously set in both the question string
+      and the options so react would render react entities (e.g. &gt; 
+      correctly) */}
       <h2 dangerouslySetInnerHTML={{ __html: trivia.question }}></h2>
+
       <div className="options-group">
         {trivia.options.map((option) => {
           return (
             <label key={nanoid()} className="container">
               <span dangerouslySetInnerHTML={{ __html: option }}></span>
+
               <input
                 type="radio"
                 name={trivia.question}
