7bb57e0d96f312ba2c2dcbb32c2888834523eece
Uyi <ihazauyi@gmail.com>
2022-06-16 21:16:31 +0100

    style: Commenting, style changes

--- src/components/QuizzicalContext.js
+++ src/components/QuizzicalContext.js
@@ -3,11 +3,13 @@ import { DEFAULT_STATE, quizzicalReducer } from "./reducer";
 
 const QuizzicalContext = React.createContext();
 
+/* The provider provided here wraps the entire <App /> component providing
+state and dispatch to the entire pages component. */
 const QuizzicalProvider = (props) => {
   const [state, dispatch] = React.useReducer(quizzicalReducer, DEFAULT_STATE);
 
   return (
-    <QuizzicalContext.Provider value={{ state, dispatch}}>
+    <QuizzicalContext.Provider value={{ state, dispatch }}>
       {props.children}
     </QuizzicalContext.Provider>
   );
