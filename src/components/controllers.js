ea5ac84223a0f88eb7a99ebb2aa8b2e44d73a6ac
Uyi <ihazauyi@gmail.com>
2022-06-16 21:22:54 +0100

    style: Commenting controllers

--- src/components/controllers.js
+++ src/components/controllers.js
@@ -1,17 +1,20 @@
-//import { nanoid } from "nanoid";
 const { nanoid } = require("nanoid");
-const API_BASE_URL = "https://opentdb.com/api.php";
 
+/* Used by the /settings and /game to get a new game by
+providing the api url parameters. It returns the new game object in the 
+way understood by the program  */
 const getGame = async (url) => {
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
+
     const result = await response.json();
     if (result.response_code !== 0) {
       throw new Error("There was an error in response");
     }
+
     const game = createGame(result.results);
     return game;
   } catch (error) {
@@ -20,13 +23,19 @@ const getGame = async (url) => {
   }
 };
 
+/* Rearrange the received game object in a way that is understood
+by the program */
 const createGame = (apiResult) => {
   const game = apiResult.map((result) => {
     const { category, difficulty, question, correct_answer } = result;
+
+    /* Add all the options together and randomly sort them */
     const options = result.incorrect_answers.concat(correct_answer);
     options.sort((a, b) => 0.5 - Math.random());
-    const selected = "";
-    const id = nanoid();
+
+    const selected = ""; // The option will be selected during the game
+    const id = nanoid(); // Each question has a unique id
+
     return {
       question,
       correct_answer,
@@ -37,9 +46,12 @@ const createGame = (apiResult) => {
       id,
     };
   });
+
   return game;
 };
 
+/* Converts from minutes (which the user will choose) to ms (which is 
+  understood by the timer) AND formats ms to mm:ss  */
 const TimeConverter = {
   minutesToMillis(minutes) {
     return minutes * 60 * 1000;
